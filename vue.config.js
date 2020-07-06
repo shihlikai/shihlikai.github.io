30f2b79ec2669019bee00438693f1b9c3682aa23
William Shih <jlk.william@simbo.com.tw>
2020-07-06 20:31:28 +0800

    update dist

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const app = require('./src/app.config')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
   chainWebpack: config => {
     config.plugin('html')
       .tap(args => {
