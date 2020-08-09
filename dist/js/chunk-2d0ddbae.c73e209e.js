a063e5f908e1d45b59188231f626a83fdf241038
William Shih <jlk.william@simbo.com.tw>
2020-08-09 18:52:56 +0800

    update dist

---
+++ dist/js/chunk-2d0ddbae.c73e209e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ddbae"],{8341:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},a=[],r={name:"AppMain"},c=r,i=t("2877"),l=Object(i["a"])(c,u,a,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0ddbae.c73e209e.js.map
